de0a410feb78fe210dab5cb02641ea9a8a820a25
Nahida4479 <szymon.cyb13@gmail.com>
2026-07-27 23:25:27 +0200

    Create frontend upload data system

--- public/script.js
+++ public/script.js
@@ -6,6 +6,16 @@ const outputdownloadbutton = document.getElementById('downloadoutput')
 
 convertbutton.addEventListener("click", async function() {
     const useruploadfile = uploadfile.files[0];
+
     const formDatatoServer = new FormData();
-    formDatatoServer.append('video', useruploadfile);
+    formDatatoServer.append('video', useruploadfile); // User upload file
+    formDatatoServer.append('resolution', videoresolutionlist.value); 
+    formDatatoServer.append('format', fileformat.value);
+
+    const response = await fetch('/convert', {
+       method: 'POST',
+       body: formDatatoServer 
+    });
+
+
 });
