19c4bfce233cda66057157066dd2cb62420ed6ca
Nahida4479 <szymon.cyb13@gmail.com>
2026-07-28 00:05:45 +0200

    Add /convert download system

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/'});
-
 const app = express();
 
-app.use((req, res, next) => {
-res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-next();
-});
-
 app.use(express.static('./public'))
+app.post('/convert', upload.single('video'), function(req, res) {
+    console.log(req.file)
+    console.log(res.body)
+});
 
 app.listen(3003, () => {
     console.log("Active: Port 3003")
