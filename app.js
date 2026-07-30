677bdb9676bc10f7d689f98be4d514e3fd820fda
Nahida4479 <szymon.cyb13@gmail.com>
2026-07-30 21:10:50 +0200

    Install ffmpeg-static and ffprobe-static

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const multer = require('multer');
+const ffmpegPath = require('ffmpeg-static')
 const { execFile } = require('child_process');
+const ffmpeg_probe = require('ffprobe-static');
 const upload = multer({ dest: 'uploads/'});
 const app = express();
 
@@ -8,7 +10,16 @@ app.use(express.static('./public'))
 app.post('/convert', upload.single('video'), function(req, res) {
     console.log(req.file)
     console.log(res.body)
-    res.send('Plik odebrany!');
+    const inputPath = req.file.path
+    const nameParts = req.file.originalname.split('.');
+    const basefileextenstion = nameParts[nameParts.length - 1];
+    const outputfile = `uploads/output-${req.file.filename}.${req.body.format}`;
+
+    execFile(ffmpegPath, ['-i', inputPath, outputfile], function(error, stdout, stderr) {
+        console.log(error)
+        res.send('Plik odebrany!');
+    });
+
 });
 
 app.listen(3003, () => {
