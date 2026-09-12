f8f7b66a8625a6125d396c460022bd5c87837fa8
Nahida4479 <szymon.cyb13@gmail.com>
2026-09-12 18:59:10 +0200

    Change /app to /FFvert in yt-dlp.js

--- services/yt-dlp.js
+++ services/yt-dlp.js
@@ -26,7 +26,7 @@ await youtubedl(url, options);
 return outputPath.replace('%(ext)s', 'mp4');
 }
 
-const firefoxProfilePath = '/app/ff-profile';u
+const firefoxProfilePath = '/FFvert/ff-profile';u
 if (fs.existsSync(path.join(firefoxProfilePath, 'cookies.sqlite'))) {
     options.cookiesFromBrowser = `firefox:${firefoxProfilePath}`
 }
