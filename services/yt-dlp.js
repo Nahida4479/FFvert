8d493cf060ce5ffad856a89425b5d808a80962cb
Nahida4479 <szymon.cyb13@gmail.com>
2026-09-12 19:35:48 +0200

    Fix cookies issues

--- services/yt-dlp.js
+++ services/yt-dlp.js
@@ -21,14 +21,15 @@ if (fs.existsSync(cookiesPath)) {
 options.cookies = cookiesPath;
 }
 
-await youtubedl(url, options);
-
-return outputPath.replace('%(ext)s', 'mp4');
-}
-
 const firefoxProfilePath = '/FFvert/ff-profile';
 if (fs.existsSync(path.join(firefoxProfilePath, 'cookies.sqlite'))) {
     options.cookiesFromBrowser = `firefox:${firefoxProfilePath}`
+}
+
+
+await youtubedl(url, options);
+
+return outputPath.replace('%(ext)s', 'mp4');
 }
 
 module.exports = {downloadVideo}
